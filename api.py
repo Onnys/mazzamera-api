83a08acd408496418ecb6e163678c83aa4655ce8
onnys <onnysanild@gmail.com>
2020-11-13 00:10:01 +0200

    first commit

---
+++ api.py
@@ -0,0 +1,24 @@
+from flask import  Flask, request, url_for
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+@app.route('/')
+def index():
+    return 'Hello world'
+
+
+
+@app.route('/mazzameras', methods=['POST'])
+def upload_file():
+    image = request.files['image']
+    filename = secure_filename(image.filename)
+    url = os.path.join(APP_ROOT,filename) 
+    
+    image.save(url)
+    return url
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
