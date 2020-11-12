d5558fe884058e138a24fffa5d8f20d53d912abd
onnys <onnysanild@gmail.com>
2020-11-13 01:10:03 +0200

    Refractored code

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import  Flask, request
 import os
 
-app = Flask(__name__)
-#APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route('/')
-def index():
-    return 'Hello world'
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return 'Hello world'
+
+    @app.route('/mazzameras', methods=['POST'])
+    def upload_file():
+        image = request.files['image']
+        filename = secure_filename(image.filename)
+        url = os.path.join(APP_ROOT,filename)
+
+        image.save(url)
+        return url
+
+    return app
+
+app = create_app()
 
 
-
-# @app.route('/mazzameras', methods=['POST'])
-# def upload_file():
-#     image = request.files['image']
-#     filename = secure_filename(image.filename)
-#     url = os.path.join(APP_ROOT,filename)
-#
-#     image.save(url)
-#     return url
-
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
