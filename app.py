80697622b1d207c62961eedf154a108f33374842
onnys <onnysanild@gmail.com>
2020-11-14 18:01:33 +0200

    POST files endpoint

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import  Flask, request
+from flask import  Flask, request, send_from_directory, safe_join, abort , jsonify
 import os
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_ROOT = (os.path.dirname(os.path.abspath(__file__))+'/static')
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -10,14 +11,23 @@ def create_app(test_config=None):
     def index():
         return 'Hello world'
 
-    @app.route('/mazzameras', methods=['POST'])
+    @app.route('/add-mazzameras', methods=['POST'])
     def upload_file():
         image = request.files['image']
-        filename = secure_filename(image.filename)
-        url = os.path.join(APP_ROOT,filename)
-
+        url = os.path.join(APP_ROOT,image.filename)
         image.save(url)
-        return url
+        return jsonify({
+            'success': True,
+            'file_name':image.filename,
+        })
+
+    @app.route('/get-mazzamera', methods=['GET'])
+    def get_image():
+        try:
+            return send_from_directory(directory='static',filename='Phoenix Rdc - Vencedor.mp4')
+
+        except FileNotFoundError:
+            abort(404)
 
     return app
 
